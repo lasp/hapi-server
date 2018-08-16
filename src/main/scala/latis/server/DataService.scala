5c47ebb1b2439b6a0929c1f0bf1295781c5eb0bd
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2018-08-16 11:30:01 -0600

    Add type representing data requests
    See #9.

--- src/main/scala/latis/server/DataService.scala
+++ src/main/scala/latis/server/DataService.scala
@@ -13,3 +13,22 @@ class DataService[F[_]: Effect] extends Http4sDsl[F] {
         Ok("Hello from HAPI!")
     }
 }
+
+/**
+ * Represents a request for data.
+ *
+ * @param id HAPI dataset ID
+ * @param minTime lower time bound (inclusive)
+ * @param maxTime upper time bound (exclusive)
+ * @param parameters list of parameter names to return
+ * @param header whether to include the metadata header
+ * @param format output format
+ */
+final case class DataRequest(
+  id: String,
+  minTime: String,
+  maxTime: String,
+  parameters: Option[List[String]],
+  header: Boolean,
+  format: String
+)
