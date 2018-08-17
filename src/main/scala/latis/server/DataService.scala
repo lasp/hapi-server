81116425974f141629edf5edca8b77211cbe23f0
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2018-08-17 16:16:01 -0600

    Add query parameter decoder instances for data queries
    This will need to be refactored. Some of this stuff will probably be
    shared with the 'info' endpoint, so it's only here temporarily.
    
    See #9.

--- src/main/scala/latis/server/DataService.scala
+++ src/main/scala/latis/server/DataService.scala
@@ -1,8 +1,13 @@
 package latis.server
 
 import cats.effect.Effect
+import cats.implicits._
 import org.http4s.HttpService
+import org.http4s.ParseFailure
+import org.http4s.QueryParamDecoder
+import org.http4s.QueryParameterValue
 import org.http4s.dsl.Http4sDsl
+import org.http4s.dsl.io._
 
 /** Implements the `/data` endpoint. */
 class DataService[F[_]: Effect] extends Http4sDsl[F] {
@@ -32,3 +37,64 @@ final case class DataRequest(
   header: Boolean,
   format: String
 )
+
+/** Wrapper for `include` parameter. */
+final case class Include(header: Boolean) extends AnyVal
+
+object Include {
+
+  implicit val includeDecoder: QueryParamDecoder[Include] =
+    new QueryParamDecoder[Include] {
+      override def decode(qpv: QueryParameterValue) = qpv.value match {
+        case "header" => Include(true).validNel
+        case _ =>
+          ParseFailure(
+            "Invalid value for 'include' parameter",
+            s"Invalid value for 'include' parameter: ${qpv.value}"
+          ).invalidNel
+      }
+    }
+}
+
+/** Wrapper for `format` parameter. */
+final case class Format(format: String) extends AnyVal
+
+object Format {
+
+  implicit val formatDecoder: QueryParamDecoder[Format] =
+    new QueryParamDecoder[Format] {
+      override def decode(qpv: QueryParameterValue) = qpv.value match {
+        case fmt @ ("csv" | "binary" | "json") => Format(fmt).validNel
+        case _ =>
+          ParseFailure(
+            "Invalid value for 'format' parameter",
+            s"Invalid value for 'format' parameter: ${qpv.value}"
+          ).invalidNel
+      }
+    }
+}
+
+object QueryDecoders {
+
+  object IdMatcher extends QueryParamDecoderMatcher[String]("id")
+  object MinTimeMatcher extends QueryParamDecoderMatcher[String]("time.min")
+  object MaxTimeMatcher extends QueryParamDecoderMatcher[String]("time.max")
+  object ParamMatcher extends OptionalQueryParamDecoderMatcher[List[String]]("parameters")
+  object IncludeMatcher extends OptionalQueryParamDecoderMatcher[Include]("include")
+  object FormatMatcher extends OptionalQueryParamDecoderMatcher[Format]("format")
+
+  /** Decoder for simple CSV query parameters. */
+  implicit def csvDecoder[A : QueryParamDecoder]: QueryParamDecoder[List[A]] =
+    new QueryParamDecoder[List[A]] {
+      override def decode(qpv: QueryParameterValue) =
+        if (qpv.value.isEmpty) {
+          ParseFailure(
+            "Empty parameter list.", "Empty parameter list."
+          ).invalidNel
+        } else {
+          qpv.value.split(',').toList.traverse { x =>
+            QueryParamDecoder[A].decode(QueryParameterValue(x))
+          }
+        }
+    }
+}
