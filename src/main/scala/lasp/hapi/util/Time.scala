7508b55aec140892fc82a57ec4b3e1551edf5178
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2018-08-29 14:55:16 -0600

    Add HAPI time string parser
    The HAPI spec specifies that time strings are expected to conform to a
    subset of ISO 8601. The formatters added by this commit should accept
    and produce strings consistent with the spec.
    
    See #26.

---
+++ src/main/scala/lasp/hapi/util/Time.scala
@@ -0,0 +1,55 @@
+package lasp.hapi.util
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.format.DateTimeParseException
+import java.time.temporal.ChronoField.DAY_OF_MONTH
+import java.time.temporal.ChronoField.HOUR_OF_DAY
+import java.time.temporal.ChronoField.MINUTE_OF_HOUR
+import java.time.temporal.ChronoField.MONTH_OF_YEAR
+import java.time.temporal.ChronoField.NANO_OF_SECOND
+import java.time.temporal.ChronoField.SECOND_OF_MINUTE
+
+/** Time utilties. */
+object Time {
+
+  /**
+   * Parses a HAPI time string.
+   *
+   * @param str HAPI time string
+   */
+  def parse(str: String): Option[LocalDateTime] =
+    tryParse(str, format) orElse tryParse(str, formatOrd)
+
+  private def tryParse(str: String, fmt: DateTimeFormatter): Option[LocalDateTime] =
+    try {
+      Option(LocalDateTime.parse(str, fmt))
+    } catch {
+      case _: DateTimeParseException => None
+    }
+
+  /** Format for normal date/time strings. */
+  private val format: DateTimeFormatter =
+    new DateTimeFormatterBuilder()
+      .appendPattern("yyyy[-MM[-dd['T'HH[:mm[:ss[.SSS]]]]]]['Z']")
+      .parseDefaulting(MONTH_OF_YEAR, 1)
+      .parseDefaulting(DAY_OF_MONTH, 1)
+      .parseDefaulting(HOUR_OF_DAY, 0)
+      .parseDefaulting(MINUTE_OF_HOUR, 0)
+      .parseDefaulting(SECOND_OF_MINUTE, 0)
+      .parseDefaulting(NANO_OF_SECOND, 0)
+      .toFormatter()
+
+  /** Format for ordinal date/time strings. */
+  private val formatOrd: DateTimeFormatter =
+    new DateTimeFormatterBuilder()
+      .appendPattern("yyyy[-DDD['T'HH[:mm[:ss[.SSS]]]]]['Z']")
+      .parseDefaulting(MONTH_OF_YEAR, 1)
+      .parseDefaulting(DAY_OF_MONTH, 1)
+      .parseDefaulting(HOUR_OF_DAY, 0)
+      .parseDefaulting(MINUTE_OF_HOUR, 0)
+      .parseDefaulting(SECOND_OF_MINUTE, 0)
+      .parseDefaulting(NANO_OF_SECOND, 0)
+      .toFormatter()
+}
