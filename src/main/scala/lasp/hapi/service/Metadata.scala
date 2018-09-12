7d83629132bbd550f64a51decc16fa87439176d7
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2018-09-12 14:23:01 -0600

    Separate dataset metadata from InfoResponse type
    The `InfoResponse` type is intended to represent the service's
    response to an `info` request. Logically, the response is the version
    of the HAPI server, the status code, and the dataset metadata. This
    commit changes the `InfoResponse` type to better reflect that.
    
    By having a separate `Metadata` type, interpreters of `InfoAlgebra`
    need only be concerned about producing an instance of `Metadata`
    rather than the entirety of `InfoResponse`. This is similar to how
    interpreters of `CatalogAlgebra` need only produce `List[Dataset]`
    rather than `Catalog` (though the naming here is poor, we should
    consider renaming that to `CatalogResponse`).
    
    The `format` field is an unfortunate case. The spec includes it in the
    `info` response when it's only required when this information is part
    of a `data` response. Because this is `InfoResponse`, I've removed it.
    Perhaps a better way of thinking about a `data` response is that it
    optionally has the format of the data and the metadata.

---
+++ src/main/scala/lasp/hapi/service/Metadata.scala
@@ -0,0 +1,36 @@
+package lasp.hapi.service
+
+import cats.data.NonEmptyList
+
+/**
+ * Represents metadata for HAPI datasets.
+ *
+ * @param parameters selected parameters in dataset
+ * @param startDate time of first sample in dataset (restricted ISO 8601)
+ * @param stopDate time of last sample in dataset (restricted ISO 8601)
+ * @param timeStampLocation location of time stamp within measurement window
+ * @param cadence time cadence (ISO 8601 duration)
+ * @param sampleStartDate time of first sample in example subset (restricted ISO 8601)
+ * @param sampleStopDate time of last sample in example subset (restricted ISO 8601)
+ * @param description description of dataset
+ * @param resourceURL URL with more information about dataset
+ * @param creationDate time of dataset creation (restricted ISO 8601)
+ * @param modificationDate time of last modification (restricted ISO 8601)
+ * @param contact information about contact person
+ * @param contactID ID in discovery system for contact person
+ */
+final case class Metadata(
+  parameters: NonEmptyList[Parameter],
+  startDate: String,
+  stopDate: String,
+  timeStampLocation: Option[TimeStampLocation],
+  cadence: Option[String],
+  sampleStartDate: Option[String],
+  sampleStopDate: Option[String],
+  description: Option[String],
+  resourceURL: Option[String],
+  creationDate: Option[String],
+  modificationDate: Option[String],
+  contact: Option[String],
+  contactID: Option[String]
+)
