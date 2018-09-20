6d94b21255ceef27c0d321be31e31dab8d94a092
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2018-09-20 11:33:44 -0600

    Get LaTiS 2 catalog from system properties
    We no longer need the `dir` argument for the catalog location now that
    we have some sort of configuration in place. The catalog location can
    be figured out from the configuration.
    
    I would generally get configuration from values passed to the
    constructor, but LaTiS 2 typically doesn't work this way. Rather than
    risk giving two contradictory configurations, I'll just remove this
    argument.

--- src/main/scala/lasp/hapi/service/latis2/Latis2Interpreter.scala
+++ src/main/scala/lasp/hapi/service/latis2/Latis2Interpreter.scala
@@ -11,18 +11,12 @@ import latis.ops.Operation
 import latis.ops.Projection
 import latis.reader.CatalogReader
 
-/**
- * Interpreter for HAPI algebras using LaTiS 2.
- *
- * @constructor Create an interpreter that reads the catalog from the
- *              given directory.
- * @param dir directory that is the root of the catalog
- */
-class Latis2Interpreter[F[_]: Sync](dir: String) extends HapiInterpreter[F] {
+/** Interpreter for HAPI algebras using LaTiS 2. */
+class Latis2Interpreter[F[_]: Sync] extends HapiInterpreter[F] {
 
   override val getCatalog: F[List[Dataset]] =
     Sync[F].delay {
-      new CatalogReader(dir).getDataset() match {
+      new CatalogReader().getDataset() match {
         case dm.Dataset(Function(it)) =>
           it.map {
             case Sample(Text(name), _) =>
