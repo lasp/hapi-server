8ed3b3041c3e4540180346900573c9a9449239fa
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2018-10-01 18:21:18 -0600

    Clean up JSON for bins
    Bins have optional parameters that should be removed from the JSON if
    not specified.
    
    However, the `centers` parameter is special in that a null is expected
    if `ranges` is not set (but removed from the JSON) to indicate that
    the dimension corresponding to the bin does not represent binned data.

--- src/main/scala/lasp/hapi/service/Bin.scala
+++ src/main/scala/lasp/hapi/service/Bin.scala
@@ -1,13 +1,16 @@
 package lasp.hapi.service
 
 import io.circe.Encoder
+import io.circe.generic.semiauto._
 
 /**
  * Represents a parameter's bin.
  *
+ * At least one of `centers` or `ranges` must be given.
+ *
  * @param name name of dimension
- * @param centers center of bins (only if `ranges` is not defined)
- * @param ranges boundaries of each bin (only if `centers` is not defined)
+ * @param centers center of bins
+ * @param ranges boundaries of each bin
  * @param units units for bin ranges or center values
  * @param description description of bin
  */
@@ -23,9 +26,15 @@ object Bin {
 
   /** JSON encoder */
   implicit val encoder: Encoder[Bin] =
-    Encoder.forProduct5(
-      "name", "centers", "ranges", "units", "description"
-    ) { x =>
-      (x.name, x.centers, x.ranges, x.units, x.description)
+    deriveEncoder[Bin].mapJsonObject { obj =>
+      obj.filter {
+        case ("centers", _) =>
+          // Keep "centers" regardless of its value if "ranges" is
+          // null or missing.
+          obj("ranges").map(_.isNull).getOrElse(true)
+        case (_, v)         =>
+          // Remove all other null fields.
+          !v.isNull
+      }
     }
 }
