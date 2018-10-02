0b88a39e22a8935f585748e64bf7be6c2ec9d94a
Chris Lindholm <chris.lindholm@lasp.colorado.edu>
2018-10-01 18:21:18 -0600

    Clean up JSON for bins
    Bins have optional parameters that should be removed from the JSON if
    not specified.
    
    However, the `centers` parameter is special in that a null is expected
    if `ranges` is not set (but removed from the JSON) to indicate that
    the dimension corresponding to the bin does not represent binned data.

---
+++ src/test/scala/lasp/hapi/service/BinSpec.scala
@@ -0,0 +1,46 @@
+package lasp.hapi.service
+
+import io.circe._
+import io.circe.syntax._
+import org.scalatest.FlatSpec
+
+class BinSpec extends FlatSpec {
+
+  "The Bin encoder" should "keep 'centers' if 'ranges' is not defined" in {
+    val bin = Bin("", None, None, "", Option(""))
+
+    val expected = Json.obj(
+      ("name", "".asJson),
+      ("centers", Json.Null),
+      ("units", "".asJson),
+      ("description", "".asJson)
+    )
+
+    assert(bin.asJson == expected)
+  }
+
+  it should "remove 'centers' if null and 'ranges' is defined" in {
+    val bin = Bin("", None, Option(List((1,2))), "", Option(""))
+
+    val expected = Json.obj(
+      ("name", "".asJson),
+      ("ranges", Json.arr(Json.arr(1.asJson, 2.asJson))),
+      ("units", "".asJson),
+      ("description", "".asJson)
+    )
+
+    assert(bin.asJson == expected)
+  }
+
+  it should "remove null values otherwise" in {
+    val bin = Bin("", None, None, "", None)
+
+    val expected = Json.obj(
+      ("name", "".asJson),
+      ("centers", Json.Null),
+      ("units", "".asJson),
+    )
+
+    assert(bin.asJson == expected)
+  }
+}
