54864ea20c983c707231e6a7e8bc1ebcc96ec637
Jake Martin <jcmartin987@gmail.com>
2021-08-19 15:54:34 -0600

    Change Format to a sum type with constructors for Csv, Binary, and Json

--- src/main/scala/latis/service/hapi/Format.scala
+++ src/main/scala/latis/service/hapi/Format.scala
@@ -7,14 +7,25 @@ import org.http4s.QueryParameterValue
 import org.http4s.dsl.io._
 
 /** Wrapper for `format` parameter. */
-final case class Format(format: String) extends AnyVal
+sealed trait Format {
+  val format = ""
+}
 
 object Format {
+  final case object Csv extends Format {
+    override val format = "csv"
+  }
+  final case object Binary extends Format {
+    override val format = "binary"
+  }
+  final case object Json extends Format {
+    override val format = "json"
+  }
 
   implicit val formatDecoder: QueryParamDecoder[Format] =
     new QueryParamDecoder[Format] {
       override def decode(qpv: QueryParameterValue) = qpv.value match {
-        case fmt @ "csv" => Format(fmt).validNel
+        case "csv" => Csv.validNel
         case _ =>
           ParseFailure(
             "Invalid value for 'format' parameter",
