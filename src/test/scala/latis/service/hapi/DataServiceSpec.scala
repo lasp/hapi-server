73545527d9afe9e378b8de543a1586e499c53b56
Jake Martin <jcmartin987@gmail.com>
2021-10-01 15:06:16 -0600

    Make new DataService tests

--- src/test/scala/latis/service/hapi/DataServiceSpec.scala
+++ src/test/scala/latis/service/hapi/DataServiceSpec.scala
@@ -5,16 +5,45 @@ import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import cats.syntax.all._
 import io.circe.syntax._
-import org.http4s._
+import org.http4s.{Status => _, _}
 import org.http4s.implicits._
 import org.scalatest.Assertion
 import org.scalatest.FlatSpec
+import org.typelevel.ci.CIString
+import scodec.codecs
 
+import latis.catalog.Catalog
+import latis.data.DomainData
+import latis.data.RangeData
+import latis.data.Sample
+import latis.data.SeqFunction
+import latis.dataset.MemoizedDataset
+import latis.metadata.Metadata
+import latis.model._
 import latis.service.hapi.HapiError._
 import latis.service.hapi.HapiInterpreter.noopInterpreter
 import latis.service.hapi.{Status => HStatus}
+import latis.time.Time
+import latis.util.Identifier.IdentifierStringContext
 
 class DataServiceSpec extends FlatSpec {
+
+  /** Build a simple test DataService[IO] with a time -> int dataset using the Latis3Interpreter*/
+  private lazy val dataset = (for {
+    time <- Time.fromMetadata(Metadata("id"->"time", "type"->"string", "size"->"24", "units"->"yyyy-MM-dd", "coverage"->"2000-01-01/2000-01-05"))
+    disp <- Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters"))
+    model <- Function.from(id"testfunc", time, disp)
+    data = new SeqFunction(Seq(
+      Sample(DomainData("2000-01-01"), RangeData(1)),
+      Sample(DomainData("2000-01-02"), RangeData(5)),
+      Sample(DomainData("2000-01-03"), RangeData(4)),
+      Sample(DomainData("2000-01-04"), RangeData(2)),
+      Sample(DomainData("2000-01-05"), RangeData(0)),
+    ))
+    dataset = new MemoizedDataset(Metadata("id"->"testdataset"), model, data)
+  } yield dataset).fold(err => fail(err.message), identity)
+  private lazy val latisInterp = new Latis3Interpreter(Catalog(dataset))
+  private lazy val dataService = new DataService[IO](latisInterp).service
 
   /** Assert GET request to given URI returns a particular status. */
   def assertStatus(uri: Uri, status: HStatus): Assertion = {
@@ -166,5 +195,48 @@ class DataServiceSpec extends FlatSpec {
 
   it should "reject trailing commas" in {
     csvDecoderReject("a,b,")
+  }
+
+  "The DataService" should "correctly generate a response for a CSV request" in {
+    val req = Request[IO](Method.GET, uri"/data?dataset=testdataset&start=2000-01-01&stop=2000-01-06&format=csv")
+    val resp = dataService.orNotFound(req)
+    (for {
+      body <- resp.flatMap { res =>
+        res.bodyText.compile.toList
+      }
+      contentType = resp.unsafeRunSync().headers.get(CIString("Content-Type")).get.head.value
+    } yield {
+      val testStr = "2000-01-01T00:00:00.000Z,1\n" +
+                    "2000-01-02T00:00:00.000Z,5\n" +
+                    "2000-01-03T00:00:00.000Z,4\n" +
+                    "2000-01-04T00:00:00.000Z,2\n" +
+                    "2000-01-05T00:00:00.000Z,0\n"
+      assert(body.mkString == testStr)
+      assert(contentType == "text/csv")
+    }).unsafeRunSync()
+  }
+
+  it should "correctly generate a response for a binary request" in {
+    val req = Request[IO](Method.GET, uri"/data?dataset=testdataset&start=2000-01-01&stop=2000-01-06&format=binary")
+    val resp = dataService.orNotFound(req)
+    (for {
+      body <- resp.flatMap { res =>
+        res.body.compile.toList
+      }
+      contentType = resp.unsafeRunSync().headers.get(CIString("Content-Type")).get.head.value
+    } yield {
+      val encoder = codecs.list(codecs.utf8 ~ codecs.int32L)
+      val testBin = encoder.encode(
+        List(
+          ("2000-01-01T00:00:00.000Z", 1),
+          ("2000-01-02T00:00:00.000Z", 5),
+          ("2000-01-03T00:00:00.000Z", 4),
+          ("2000-01-04T00:00:00.000Z", 2),
+          ("2000-01-05T00:00:00.000Z", 0)
+        )
+      ).require.toByteArray.toList
+      assert(body == testBin)
+      assert(contentType == "application/octet-stream")
+    }).unsafeRunSync()
   }
 }
