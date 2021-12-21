c1b5d4813b785bb6e165d092ca79d0a6ed27f72e
Jake Martin <jcmartin987@gmail.com>
2021-12-21 13:50:05 -0700

    Remove unneeded usage of .right before EitherValues' .value

--- src/test/scala/latis/service/hapi/ToHapiTimeSpec.scala
+++ src/test/scala/latis/service/hapi/ToHapiTimeSpec.scala
@@ -19,9 +19,9 @@ class ToHapiTimeSpec extends AnyFlatSpec{
   private lazy val toHapiTime = new ToHapiTime
 
   "The ToHapiTime operation" should "expand yyyy-MM-dd" in {
-    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"string", "units"->"yyyy-MM-dd", "coverage"->"2000-01-01/2000-01-02")).right.value
-    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).right.value
-    val model = Function.from(time, disp).right.value
+    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"string", "units"->"yyyy-MM-dd", "coverage"->"2000-01-01/2000-01-02")).value
+    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).value
+    val model = Function.from(time, disp).value
     val data = new SeqFunction(Seq(
       Sample(DomainData("2000-01-01"), RangeData(1)),
     ))
@@ -37,9 +37,9 @@ class ToHapiTimeSpec extends AnyFlatSpec{
   }
 
   it should "handle full length yyyy-MM-ddTHH:mm:ss.SSSZ" in {
-    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"string", "units"->"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "coverage"->"2000-01-01T00:00:00.000Z/2000-01-02T00:00:00.000Z")).right.value
-    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).right.value
-    val model = Function.from(time, disp).right.value
+    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"string", "units"->"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "coverage"->"2000-01-01T00:00:00.000Z/2000-01-02T00:00:00.000Z")).value
+    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).value
+    val model = Function.from(time, disp).value
     val data = new SeqFunction(Seq(
       Sample(DomainData("2000-01-01T01:01:01.001Z"), RangeData(1)),
     ))
@@ -55,9 +55,9 @@ class ToHapiTimeSpec extends AnyFlatSpec{
   }
 
   it should "expand yyyy-ddd" in {
-    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"string", "units"->"yyyy-ddd", "coverage"->"2000-001/2000-002")).right.value
-    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).right.value
-    val model = Function.from(time, disp).right.value
+    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"string", "units"->"yyyy-ddd", "coverage"->"2000-001/2000-002")).value
+    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).value
+    val model = Function.from(time, disp).value
     val data = new SeqFunction(Seq(
       Sample(DomainData("2000-001"), RangeData(1)),
     ))
@@ -73,9 +73,9 @@ class ToHapiTimeSpec extends AnyFlatSpec{
   }
 
   it should "handle full length yyyy-dddTHH:mm:ss.SSSZ" in {
-    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"string", "units"->"yyyy-ddd'T'HH:mm:ss.SSS'Z'", "coverage"->"2000-001T00:00:00.000Z/2000-002T00:00:00.000Z")).right.value
-    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).right.value
-    val model = Function.from(time, disp).right.value
+    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"string", "units"->"yyyy-ddd'T'HH:mm:ss.SSS'Z'", "coverage"->"2000-001T00:00:00.000Z/2000-002T00:00:00.000Z")).value
+    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).value
+    val model = Function.from(time, disp).value
     val data = new SeqFunction(Seq(
       Sample(DomainData("2000-001T01:01:01.001Z"), RangeData(1)),
     ))
@@ -91,9 +91,9 @@ class ToHapiTimeSpec extends AnyFlatSpec{
   }
 
   it should "handle time in seconds since 2000" in {
-    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"double", "units"->"seconds since 2000-01-01", "coverage"->"0/10000")).right.value
-    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).right.value
-    val model = Function.from(time, disp).right.value
+    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"double", "units"->"seconds since 2000-01-01", "coverage"->"0/10000")).value
+    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).value
+    val model = Function.from(time, disp).value
     val data = new SeqFunction(Seq(
       Sample(time.convertValue("3661.001").toSeq, RangeData(1)),
     ))
@@ -109,9 +109,9 @@ class ToHapiTimeSpec extends AnyFlatSpec{
   }
 
   it should "handle time in milliseconds since 2000" in {
-    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"double", "units"->"milliseconds since 2000-01-01", "coverage"->"0/10000000")).right.value
-    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).right.value
-    val model = Function.from(time, disp).right.value
+    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"double", "units"->"milliseconds since 2000-01-01", "coverage"->"0/10000000")).value
+    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).value
+    val model = Function.from(time, disp).value
     val data = new SeqFunction(Seq(
       Sample(time.convertValue("3661001").toSeq, RangeData(1)),
     ))
@@ -127,9 +127,9 @@ class ToHapiTimeSpec extends AnyFlatSpec{
   }
 
   it should "handle time in seconds since 1970" in {
-    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"double", "units"->"seconds since 1970-01-01", "coverage"->"0/1000000000")).right.value
-    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).right.value
-    val model = Function.from(time, disp).right.value
+    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"double", "units"->"seconds since 1970-01-01", "coverage"->"0/1000000000")).value
+    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).value
+    val model = Function.from(time, disp).value
     val data = new SeqFunction(Seq(
       Sample(time.convertValue("946688461.001").toSeq, RangeData(1)),
     ))
@@ -145,9 +145,9 @@ class ToHapiTimeSpec extends AnyFlatSpec{
   }
 
   it should "handle time in milliseconds since 1970" in {
-    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"double", "units"->"milliseconds since 1970-01-01", "coverage"->"0/1000000000000")).right.value
-    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).right.value
-    val model = Function.from(time, disp).right.value
+    val time = Time.fromMetadata(Metadata("id"->"time", "type"->"double", "units"->"milliseconds since 1970-01-01", "coverage"->"0/1000000000000")).value
+    val disp = Scalar.fromMetadata(Metadata("id"->"displacement", "type"->"int", "units"->"meters")).value
+    val model = Function.from(time, disp).value
     val data = new SeqFunction(Seq(
       Sample(time.convertValue("946688461001").toSeq, RangeData(1)),
     ))
