51144594a08018daa5b7b4b3ad59d7acd6c92240
Jake Martin <jcmartin987@gmail.com>
2022-02-10 16:30:34 -0700

    Add catalog table to Landing Page Service

--- src/main/scala/latis/service/hapi/LandingPageService.scala
+++ src/main/scala/latis/service/hapi/LandingPageService.scala
@@ -1,46 +1,77 @@
 package latis.service.hapi
 
 import cats.Monad
+import cats.syntax.all._
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.scalatags._
+import scalatags.Text
 import scalatags.Text.all._
 
 /** Implements the HAPI landing page. */
-class LandingPageService[F[_]: Monad] extends Http4sDsl[F] {
+class LandingPageService[F[_]: Monad](alg: CatalogAlgebra[F]) extends Http4sDsl[F] {
 
-  val landingPage =
-    html(
-      body(
-        h1("LASP HAPI Server"),
-        p("""This server supports the HAPI 3.0 specification for
-        |delivery of time series data. The server consists of the
-        |following 5 REST-like endpoints that will respond to HTTP GET
-        |requests:""".stripMargin.replaceAll("\n", " ")),
-        ul(
-          li(
-            a(href := "hapi/about")("about"),
-            " - information about the server"
-          ),
-          li(
-            a(href := "hapi/capabilities")("capabilities"),
-            " - list the output formats the server can emit"
-          ),
-          li(
-            a(href := "hapi/catalog")("catalog"),
-            " - list the datasets that are available"
-          ),
-          li(
-            a(href := "hapi/info")("info"),
-            " - describe a dataset"
-          ),
-          li(
-            a(href := "hapi/data")("data"),
-            " - stream content of a dataset"
-          )
+  val catalogTable: F[Text.TypedTag[String]] = for {
+    catalog <- alg.getCatalog
+    catalogEntries = catalog.foldLeft(frag()) { (acc, dataset) =>
+      frag(
+        acc,
+        tr(
+          td(dataset.id),
+          td(a(href := "hapi/info?dataset="+dataset.id)(dataset.title))
         )
       )
+    }
+  } yield {
+    table(
+      caption(b(i(u("Catalog")))),
+      tr(
+        th("id"),
+        th("title"),
+      ),
+      catalogEntries
     )
+  }
+
+  val landingPage: F[Text.TypedTag[String]] = {
+    for {
+      table <- catalogTable
+    } yield {
+      html(
+        body(
+          h1("LASP HAPI Server"),
+          p(
+            """This server supports the HAPI 3.0 specification for
+              |delivery of time series data. The server consists of the
+              |following 5 REST-like endpoints that will respond to HTTP GET
+              |requests:""".stripMargin.replaceAll(System.lineSeparator(), " ")),
+          ul(
+            li(
+              a(href := "hapi/about")("about"),
+              " - information about the server"
+            ),
+            li(
+              a(href := "hapi/capabilities")("capabilities"),
+              " - list the output formats the server can emit"
+            ),
+            li(
+              a(href := "hapi/catalog")("catalog"),
+              " - list the datasets that are available"
+            ),
+            li(
+              a(href := "hapi/info")("info"),
+              " - describe a dataset"
+            ),
+            li(
+              a(href := "hapi/data")("data"),
+              " - stream content of a dataset"
+            )
+          ),
+          table
+        )
+      )
+    }
+  }
 
   val service: HttpRoutes[F] =
     HttpRoutes.of[F] {
